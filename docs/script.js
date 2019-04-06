06c67a50f5fd6793bf13b049c9ace674f80f9ded
JulianGerrit <37937880+JulianGerrit@users.noreply.github.com>
2019-04-06 23:07:35 +0200

    Clicker game met cookies
    Er staat nu een prachtige foto van een frikandelbroodje waar je op kan klikken.
    Ook zijn er save, load en reset knoppen die werken met cookies.

--- docs/script.js
+++ docs/script.js
@@ -1,21 +1,77 @@
-main();
+/*
+  TODO LIST:
+    - Upgrades
+*/
 
-//
-// start here
-//
-function main() {
-  const canvas = document.querySelector("#glCanvas");
-  // Initialize the GL context
-  const gl = canvas.getContext("webgl");
+var count = 0;
+var incr = 1;
 
-  // Only continue if WebGL is available and working
-  if (gl === null) {
-    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-    return;
+
+// TODO: Animate when clicked
+function add()
+{
+  count += incr;
+  refresh();
+}
+
+function refresh()
+{
+  document.getElementById("counter").value = count;
+  // Element.value = variable
+  // Add each element containing a variable here
+}
+
+var storage = {
+
+  save: function()
+  {
+    cookies.set("frikandelbroodjes", count.toString());
+  },
+
+  load: function()
+  {
+    count = parseInt(cookies.get("frikandelbroodjes"));
+    refresh();
+  },
+
+  reset: function()
+  {
+    // Delete cookies or not???
+    count = 0;
+    incr = 1;
+    refresh();
   }
 
-  // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  // Clear the color buffer with specified clear color
-  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+var cookies = {
+
+  get: function(name)
+  {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  },
+
+  set: function(name, value, days)
+  {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+  },
+
+  delete: function(name)
+  {
+    document.cookie = name+'=; Max-Age=-99999999;';
+  }
+
 }
